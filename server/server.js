e7314869d838158b1b721870e58314aa679ceea4
Bishal Singh <72182666+Bishal091@users.noreply.github.com>
2024-11-04 13:51:33 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -7,27 +7,17 @@ const dataRouter = require('./routes/data');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const reactAppApiUrl = process.env.REACT_APP_API_URL || 'https://dataviztop.netlify.app';
+// const reactAppApiUrl = process.env.REACT_APP_API_URL || 'https://dataviztop.netlify.app';
 
-// Set allowed origins dynamically
-const allowedOrigins = [
-  reactAppApiUrl,
-  'http://localhost:5174' // Example for local development
-];
-
+// CORS
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+  // origin: 'https://graytm-wallet.netlify.app',
+  origin: "https://dataviztop.netlify.app",
+    methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+    credentials: true,
+  };
+  
+  app.use(cors(corsOptions));
 
 app.use(express.json());
 
